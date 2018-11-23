abb30673a259d2631ab6486c8f3120f1c7ac7744
Jairson Rodrigues <jairson.rodrigues@univasf.edu.br>
2018-11-22 21:58:20 -0300

    bug fixes in user-defined k values

--- demo.r
+++ demo.r
@@ -2,4 +2,6 @@ source("helpers.r")
 data("als.cpu.gig")
 
 gama.res <- gama(als.cpu.gig, plot.internals = T, generations = 100)
-plot(gama.res, view.method = "pca")
+plot(gama.res, view.method = "pca") 
+
+nkn
